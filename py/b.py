932b00d31291adf28ae193d8779a613c75ce1461
jchan60 <jchan@stuy.edu>
2024-05-31 14:33:35 -0400

    why all my tries no work

--- py/b.py
+++ py/b.py
@@ -22,15 +22,15 @@ HTML_FOOTER = """
 """
 
 data = cgi.FieldStorage()
-terms = 'a,b,c,d'
-if ('terms' in data):
+term = 'a,b,c,d'
+if ('term' in data):
     terms = data['terms'].value.split(',')
 defs = '1,2,3,4'
 if ('defs' in data):
-    defs = data['defs'].value.split(',')
+    def_list = data['defs'].value.split(',')
 
 html= HTML_HEADER
 #html+= '<br><a href="b.html">Try Again</a></br>'
-html+='<p>', terms, '</p>' , '<p>' , defs , '</p>'
+html+='<p>', terms, '</p>' , '<p>' , def_list , '</p>'
 html+= HTML_FOOTER
 print(html)
