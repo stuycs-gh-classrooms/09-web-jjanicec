5f4b94b09589c6cced2e4f0a2e13dc2c8983ac32
jjanicec <144542252+jjanicec@users.noreply.github.com>
2024-06-09 20:39:35 -0400

    Update test01.py

--- py/test01.py
+++ py/test01.py
@@ -38,7 +38,7 @@ if 'index' in data0:
 
 html = HTML_HEADER
 html += '''
-<form action="/py/test1.py" method="GET">
+<form action="/py/test01.py" method="GET">
 Next Term <input type="checkbox" name="next_term" value="yes">
 <br>
 Show Def <input type="checkbox" name="show_def" value="show_def">
