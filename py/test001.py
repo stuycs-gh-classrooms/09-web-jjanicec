82dd599d5ad470d41d8b1b803060559e8ceade88
jjanicec <144542252+jjanicec@users.noreply.github.com>
2024-06-12 10:08:13 -0400

    Update test001.py

--- py/test001.py
+++ py/test001.py
@@ -30,26 +30,23 @@ if 'defs' in data0:
     defs = data0['defs'].value
 term_list = terms.split(',')
 def_list = defs.split(',')
-next_term = 'no'
-show_def = 'no'
 
+'''
 # Initialize index i
 i = 0
 if 'index' in data0:
     i = int(data0['index'].value)
+'''
 
 html = HTML_HEADER
-html += '<p>Term: ' + term_list[i] + '</p>'
 
-if 'next_term' in data0 and data0['next_term'].value == 'yes':
+i = 0
+while i < len(term_list):
+    html += '<p>Term: ' + term_list[i] + '</p>'
+    html += '<br><p style="color:rgb(203 247 236);">Definition: ' + def_list[i] + '</p>'
     i += 1
-    if i < len(term_list):
-        html += '<p>Next Term: ' + term_list[i] + '</p>'
-    else:
-        html += '<p>No more terms.</p>'
-        i = len(term_list) - 1
-if 'show_def' in data0 and data0['show_def'].value == 'show_def':
-    if i < len(def_list):
-        html += '<p>Definition: ' + def_list[i] + '</p>'
+if 'show_def' in data0 and data['show_def'].value == 'on':
+    html.replace('rgb(203 247 236);', 'black')
+
 html += HTML_FOOTER
 print(html)
